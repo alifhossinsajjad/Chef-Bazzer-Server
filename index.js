d4d36d9b97a0aac65e47831dc8c41cba867ce8dc
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-12-06 14:18:51 +0600

    added fb servicekey

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-const express = require('express')
+const express = require("express");
 const cors = require("cors");
-const app = express()
+const app = express();
 require("dotenv").config();
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const stripe = require("stripe")(process.env.PAYMENT_GATEWAY_API_KEY);
+
+const port = process.env.port || 3000;
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const admin = require("firebase-admin");
+
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+
+const serviceAccount = JSON.parse(decoded);
 
 
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
-const port = process.env.port || 3000
-
-
+const crypto = require("crypto");
+const { log } = require("console");
 
 
 
@@ -20,37 +32,34 @@ const port = process.env.port || 3000
 app.use(express.json());
 app.use(cors());
 
-
-const uri = "mongodb+srv://alifhossinsajjad123456_db_user:msmitIoNca5nBTEf@chefdb.qtrpqbo.mongodb.net/?appName=chefDB";
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@chefdb.qtrpqbo.mongodb.net/?appName=chefDB`;
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-app.get('/', (req, res) => {
-  res.send('Chef bazzer server run successfully')
-})
+app.get("/", (req, res) => {
+  res.send("Chef bazzer server run successfully");
+});
 
 async function run() {
   try {
-    
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    
   }
 }
 run().catch(console.dir);
 
-
-
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
